165987b2fffe8ceb22391cdf9bbb3f1e55e34d04
Arian <arian.amorzegan@mail.um.ac.ir>
2024-08-15 13:14:18 +0330

    first

---
+++ src/Person.java
@@ -0,0 +1,10 @@
+public class Person {
+    String name;
+    int age;
+    int role;
+    public Person(String name, int age, int role) {
+        this.name = name;
+        this.age = age;
+        this.role = role;
+    }
+}
